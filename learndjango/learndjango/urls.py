f2ee8678c904be82de88b7294ebece79d41ce2f1
Vaibhav Desai <vaibhav.desai@thinkinghut.in>
2020-02-28 19:36:46 +1100

    basic controller

--- learndjango/learndjango/urls.py
+++ learndjango/learndjango/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from.views import (
+    home_page,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', home_page)
 ]
